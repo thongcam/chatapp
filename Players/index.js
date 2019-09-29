7de4651b2dd81e21741ce3793ed428c7df6aff34
Thong Cam <thongcam02@gmail.com>
2019-09-29 12:00:39 +0700

    Debug

--- Players/index.js
+++ Players/index.js
@@ -1,11 +1,18 @@
 $('.choice').click((event) => {
   fetch("https://chatapp-entropy.herokuapp.com/choose", {
-      method:"post",
+      method:'post',
       headers: {'Content-Type': 'application/json'},
       credentials: 'include',
       body: JSON.stringify({
         playercode: event.target.id,
         player: event.target.firstChild.nodeValue,
     })})
-    .then(setTimeout(window.location.assign('https://thongcam.github.io/chatapp/UI/index.html'),2000))
+    .then(response => response.json())
+    .then(data => {
+      if (data === 'Success') {
+        setTimeout(window.location.assign('https://thongcam.github.io/chatapp/UI/index.html'),2000)
+      } else {
+        alert('Error')
+      }
+    })
 })
